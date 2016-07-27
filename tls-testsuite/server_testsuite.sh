bd2fc673a362140ade12b6299c7df72fcaa9a96b
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-07-27 18:16:03 +0200

    Exit status from java observed

--- tls-testsuite/server_testsuite.sh
+++ tls-testsuite/server_testsuite.sh
@@ -9,5 +9,14 @@ botan/botan tls_server TLS-Attacker/resources/rsa1024cert.pem TLS-Attacker/resou
 botan_pid=$!
 
 java -jar TLS-Attacker/Runnable/target/TLS-Attacker-1.1.jar -loglevel INFO testsuite_server -folder TLS-Attacker/resources/testsuite -tls_timeout 1000
+rc=$?
+
+if [ $rc -eq 0 ]; then
+    echo Tests finished without failures
+else
+    echo '\n\nTests failed. Server log output:\n\n'
+    cat output/server_output.log
+fi
 
 kill $botan_pid
+exit $rc
